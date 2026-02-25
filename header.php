a3fd24c3fb59d55998b1b08db5333613b1a5277a
hobbsgo28 <hobbsgo28@vmi.edu>
2026-02-25 16:36:33 -0500

    main structure added

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
   <a href="index.php">Grace Hobbs</a>
   <div class="header-right">
 
+  <!-- <a href="www.linkedin.com/in/grace-hobbs-2a1329376" target="_blank" rel="noopener noreferrer"> Linkedin </a> -->
   <a href="HobbsResume2026.pdf" > Resume </a>
 
 </div>
--- index.php
+++ index.php
@@ -2,16 +2,62 @@
 <?php include("fonts.php"); ?>   
 <link rel="stylesheet" href="style.css"> 
 </head>
-<div class="container">
+<body>
 
 <?php
 include("header.php");
-// include("footer.php");
+include("footer.php");
 ?>
+<div class="container-main">
 
-<body>
+
+<div class="column-50">
+
 <h1> Grace Hobbs </h1>
 
-</body>
+</div>
+
+<div class="column-50">
+
+<h1>Picture</h1>
 
 </div>
+
+</div>
+
+<div class="container-horz">
+<div class="column-40">
+    <h1>Visual Element</h1>
+
+</div>
+<div class="column-60">
+    <h1>Skills list </h1>
+</div>
+</div>
+
+<div class="container-horz">
+<div class="column-60">
+    <h1>Experience list </h1>
+</div>
+<div class="column-40">
+    <h1>Visual Element</h1>
+
+</div>
+</div>
+
+<div class="container-horz">
+<div class="column-40">
+    <h1>Visual Element</h1>
+
+</div>
+<div class="column-60">
+    <h1>Education list </h1>
+</div>
+</div>
+
+
+
+
+
+
+</body>
