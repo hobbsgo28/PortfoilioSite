fd067487dc793eabcf709290dee4e9d33d6aac57
hobbsgo28 <hobbsgo28@vmi.edu>
2026-03-05 17:28:17 -0500

    content added

--- index.php
+++ index.php
@@ -14,6 +14,14 @@ include("header.php");
 <div class="column-50">
 
 <h1> Grace Hobbs </h1>
+<p> As a computer science student, I have developed skills in Java, Python, and assembly language. 
+    Currently, I am preparing for the A+ Core 1 and Core 2 certification, which is broadening my 
+    understanding of computer hardware, operating systems, and IT support. My practical experience in 
+    the VMI Cyber Defense Lab also provides me with valuable real-world expertise that I am eager to 
+    contribute to the workforce. The software development team has allowed me to expand my understanding 
+    of software development and to learn additional programming languages including HTML, PHP, SQL, and CSS. 
+    Working in software development at the VMI Cyber Defense Lab exposes me to how software developers avoid 
+    vulnerabilities in a system's fundamental software.</p>
 
 </div>
 
@@ -27,32 +35,48 @@ include("header.php");
 </div>
 
 <div class="container-horz">
-<div class="column-40">
-    <h1>Visual Element</h1>
+    <div class="column-40">
+        <h1>Visual Element</h1>
+    </div>
 
-</div>
 <div class="column-60">
-    <h1>Skills list </h1>
-</div>
+    <h1>Skills</h1>
+        <ul>
+            <li>Python</li>
+            <li>Java</li>
+            <li>MIPS Assembly</li>
+            <li>HTML</li>
+            <li>PHP</li>
+            <li>SQL</li>
+            <li>CSS</li>
+        </ul>
+    </div>
 </div>
 
 <div class="container-horz">
-<div class="column-60">
-    <h1>Experience list </h1>
-</div>
-<div class="column-40">
-    <h1>Visual Element</h1>
+    <div class="column-60">
+        <h1>Experience</h1>
+            <dl>
+                <dt><h3>DoD SMC Cyber Institute at Virginia Military Institute, Lexington, Virginia</h3></dt>
+                <dd>October 2025 - Present</dd>
+                <dd>Software Development Team<dd>
 
-</div>
+            </dl>
+    </div>
+    <div class="column-40">
+        <h1>Visual Element</h1>
+
+    </div>
 </div>
 
 <div class="container-horz">
-<div class="column-40">
-    <h1>Visual Element</h1>
+    <div class="column-40">
+        <h1>Visual Element</h1>
 
-</div>
-<div class="column-60">
-    <h1>Education list </h1>
+    </div>
+    <div class="column-60">
+        <h1>Education</h1>
+
 </div>
 </div>
 
